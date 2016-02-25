ca183d249b6f414f626cba11446256c624e13f5d
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2016-02-25 11:45:44 +0300

    Change order include function files

--- functions.php
+++ functions.php
@@ -1,11 +1,12 @@
 <?php
-include('functions/default-funcs.php');		// functions settings and page settings
-include('functions/customize-post.php');	// create post types, funcs for text content
-include('functions/pagination.php');		// paginatind functions
-include('functions/custom-comments.php');	// comment functions
-include('functions/social-widgets.php');	// social widgets fd/vk/subscribe
-include('functions/users-funcs.php');		// users reg/login/update/redirect
-include('functions/breadcrumbs.php');		// breadcrumbs generation
-include('functions/class-widget.php');		// sidebar widget class
-include('functions/views_counter.php');		// functions for views counter
+include('functions/breadcrumbs.php');       // breadcrumbs generation
+include('functions/class-widget.php');      // sidebar widget class
+include('functions/csv-generate.php');      // generate csv-file
+include('functions/custom-comments.php');   // comment functions
+include('functions/customize-post.php');    // create post types, funcs for text content
+include('functions/default-funcs.php');     // functions settings and page settings
+include('functions/pagination.php');        // paginatind functions
+include('functions/social-widgets.php');    // social widgets fd/vk/subscribe
+include('functions/users-funcs.php');       // users reg/login/update/redirect
+include('functions/views_counter.php');     // functions for views counter
 ?>
